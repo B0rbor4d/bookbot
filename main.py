286daa6bde9fa945407770442a6f48cb8e86b824
b0rbor4d <b0rbor4d@gmail.com>
2025-08-19 07:51:27 +0200

    finished CH3 L2 - adding commandline argument for input and instructions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -8,19 +10,24 @@ from stats import (count_words, count_letters, sort_list)
 
 
 def main():
-    path_to_file = "./books/frankenstein.txt"
-    
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path_to_file[2::]}...")
-    print(f"----------- Word Count ----------")
-    print(f"Found {count_words(get_book_text(path_to_file))} total words")
-    print(f"--------- Character Count -------")
-    #print(f"{sort_list(count_letters(get_book_text(path_to_file)))}")
+    if(len(sys.argv) < 2):
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_file = sys.argv[1]
+        
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_file}...")
+        print(f"----------- Word Count ----------")
+        print(f"Found {count_words(get_book_text(path_to_file))} total words")
+        print(f"--------- Character Count -------")
+        #print(f"{sort_list(count_letters(get_book_text(path_to_file)))}")
 
-    sorted_letters = sort_list(count_letters(get_book_text(path_to_file)))
-    for letter in sorted_letters:
-        print(f"{letter["Buchstabe"]}: {letter["Anzahl"]}")
+        sorted_letters = sort_list(count_letters(get_book_text(path_to_file)))
+        for letter in sorted_letters:
+            print(f"{letter["Buchstabe"]}: {letter["Anzahl"]}")
+        
+        print(f"============= END ===============")
     
-    print(f"============= END ===============")
 
 main()
